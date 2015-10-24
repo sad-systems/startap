cd8360080a76dbe1c2ee8e42a1136e093dae2c47
mrdigger <mrdigger@sad-systems.ru>
2015-10-24 23:19:46 +0400

    test typewriter.js as $.plugin

--- views/dummy/landing.php
+++ views/dummy/landing.php
@@ -1,8 +1,17 @@
 <div class="container-fluid">
-    <div class="jumbotron">
+    <div class="jumbotron" style="display:none;">
         <h1><?= $appInfo['title'] ?></h1>
         <h2><?= $appInfo['version'] ?></h2>
         <p><?=  $appInfo['description'] ?></p>
         <p><a class="btn btn-primary btn-lg" href="#" role="button">Welcome</a></p>
+    </div>
+    <div class="test_text" style="display:none;">
+        <table>
+        <tr><td>Value1: </td><td><a href="#w">dolor sit amet, consectetur adipisicing elit, sed</a></td></tr>
+        <tr><td>фыфыфыффы: </td><td><a href="#w">dolor sit amet, consectetur adipisicing elit, sed</a></td></tr>
+        <tr><td>Valфы фыфыфыфыф:</td><td><a href="#w">dolor sit amet, consectetur adipisicing elit, sed</a></td></tr>
+        <tr><td>чсчсчсчсчс: </td><td><a href="#w">dolor sit amet, consectetur adipisicing elit, sed</a></td></tr>
+        <tr><td>V: </td><td><a href="#w">dolor sit amet, consectetur adipisicing elit, sed</a></td></tr>
+        </table>
     </div>
 </div>
